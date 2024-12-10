88c916008cce338c089d99dbed0f766703be026c
Adwaith <91816806+adwaith02@users.noreply.github.com>
2024-12-10 12:34:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,22 +8,10 @@ const menuRoutes = require('./routes/menuRoutes');
 connectDB();
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-// Set up CORS options for allowing only specific frontend domain
-const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000', // Default to local frontend during development
-  methods: ['GET', 'POST'],
-  credentials: true, // Allow cookies if needed
-};
-
-app.use(cors(corsOptions)); // Use CORS middleware with options
-app.use(bodyParser.json()); // Middleware for parsing JSON bodies
-
-// Define routes
 app.use('/api', menuRoutes);
 
-// Dynamic port setup for production and development
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
