d367984439175490cf274608fbf8d663b09eb8eb
Adwaith <91816806+adwaith02@users.noreply.github.com>
2024-12-10 11:43:37 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://menu-backend-yjyp.onrender.com',
 });
 
 export const getMenus = () => api.get('/menus');
